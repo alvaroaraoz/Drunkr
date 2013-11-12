75c8d44c38654a38bdf65197fd8a22cc097c93d8
Alvaro Araoz <alvaroaraoz_92@hotmail.com>
2013-11-12 10:36:39 +0100

    Update nav.js

--- client/views/common/nav.js
+++ client/views/common/nav.js
@@ -15,7 +15,7 @@ Template.nav.events = {
 
 Template.nav.rendered=function(){
   if(!Meteor.user()){
-    $('.login-link-text').text("Sign Up/Sign In");
+    $('#login-buttons-logout').before('<a href="/signin" class="account-link button">Acceder</a>');
   }else{
     $('#login-buttons-logout').before('<a href="/users/'+Meteor.user().slug+'" class="account-link button">Ver Perfil</a>');
     $('#login-buttons-logout').before('<a href="/account" class="account-link button">Editar Cuenta</a>');
